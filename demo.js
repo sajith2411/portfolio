b677cd79831eb96dc2735a8ac1476b1230ad9990
sajith2411 <sajithkumar6885@gmail.com>
2024-01-28 17:16:58 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector(".navbar")
+const icon = document.querySelector(".icon")
+
+
+icon.addEventListener("click", () => {
+    navbar.classList.toggle("active")
+});
+
+
+window.addEventListener('scroll', () => {
+
+    const position = window.scrollY
+
+    if (position > 10) {
+        navbar.style.backgroundColor = 'rgba(0,0,0,0.7)';
+    } else{
+        navbar.style.backgroundColor = "transparent"
+    }
+
+
+    console.log(position);
+});
